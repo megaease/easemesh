749afbadbc96e707c933fa2e4767dc098354e649
Yun Long <longdeqidao@gmail.com>
2021-12-02 17:28:10 +0800

    delete redundant blank line

--- emctl/cmd/client/command/printer/printer.go
+++ emctl/cmd/client/command/printer/printer.go
@@ -114,7 +114,6 @@ func (p *printer) printTable(objects []meta.MeshObject) {
 }
 
 func (p *printer) printYAML(objects []meta.MeshObject) {
-	
 	yamlBuff, err := yaml.Marshal(objects)
 	if err != nil {
 		common.ExitWithErrorf("marshal %#v to yaml failed: %v", objects, err)
